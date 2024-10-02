9234616313f475fe89cb1f278e9f1cf228ec0d58
Anamtaansari <anamtaansari@csu.fullerton.edu>
2024-10-01 20:24:19 -0700

    Create user.js

---
+++ user.js
@@ -0,0 +1,13 @@
+// const { Schema } = require("mongoose");
+import {Schema, model} from "mongoose";
+
+const UserSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: Number
+})
+
+const User = model("User", UserSchema)
+
+export default User
